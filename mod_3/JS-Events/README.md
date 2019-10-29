830f85b590339c0ed2b9fa20cd1686558e30e2e4
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2019-10-29 13:00:59 -0500

    JS Events: post lecture

--- mod_3/JS-Events/README.md
+++ mod_3/JS-Events/README.md
@@ -2,19 +2,24 @@
 ---
 ## But First... a review!
 - General Q & A
+    - truthy/falsy?
+    - querySelector
 - Callbacks
+    - a function that gets passed as an argument (to another function)
 
 ## What is an "event"?
-- ...
-- ...
+- user-interaction with our page
+- a "happening" on the page
 - *Examples*:
-    - ...
-    - ...
+    - click
+    - mouseover
+    - keydown
 
 ## Let's work with Events!
-- what codebase are we working with?
-- click on document and explore events object
-- submit our form
+- explore our codebase
+- *objectives*:
+    - click on document and explore events object
+    - submit our form
 
 ## .addEventListener() on a DOM node
 - takes in a string for the name of the event, and a callback function to be invoked when the event happens
--- mod_3/JS-Events/index.js
+++ mod_3/JS-Events/index.js
@@ -1,7 +1,27 @@
 console.log("connected?");
 
+let logButton = document.querySelector("#log-button");
+function logButtonCB(){
+    console.log("clicked Middle Button");
+}
 
-// callbacks
-// "first class citizen"
-// js vs ruby: functions
-// ... 
+logButton.addEventListener("click", logButtonCB);
+
+let commentForm = document.querySelector("#comment-form");
+// debugger
+
+function formSubmissionCB(event){
+    event.preventDefault();
+    // console.log("submitting!");
+    // 1. grab the input field
+    let inputTag = document.querySelector("#new-comment");
+    // 2. grab the comment (text) from the input field
+    let commentText = inputTag.value;
+    // 3. put the text in the comment-list
+    let commentList = document.querySelector("#comments-list");
+    let listItem = document.createElement("li");
+    listItem.innerText = commentText;
+    commentList.appendChild(listItem);
+}
+
+commentForm.addEventListener("submit", formSubmissionCB );
